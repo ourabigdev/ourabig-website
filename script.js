85bb0d375c21a241ad08653012b89af478a220de
unknown <hatimourahoumoney@gmail.com>
2023-10-30 08:37:36 +0000

    adding animation

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.logo').hide().fadeIn(1000);
+    $('.me').hide().fadeIn(1000);
+    $('.learn').hide().fadeIn(1000);
+    $('.features').hide().fadeIn(1000);
+    $('.features2').hide().fadeIn(1000);
+});
